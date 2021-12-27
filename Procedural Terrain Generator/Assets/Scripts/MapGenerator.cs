a67140a95c759d7e9c404ec7afaa054980573e46
ChickenLord10 <96498268+ChickenLord10@users.noreply.github.com>
2021-12-27 21:15:09 +0100

    Tutorial Episode 1
    added:
    Simple noise map generation
    simple map display
    map editor

---
+++ Procedural Terrain Generator/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    public int MapWidth;
+    public int MapHeight;
+    public float NoiseScale;
+    public bool autoUpdate;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(MapWidth, MapHeight, NoiseScale);
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        display.DrawNoiseMap(noiseMap);
+    }
+}
